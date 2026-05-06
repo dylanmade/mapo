dadbdf58c1d469e9d4f4e2a63248658be1cc7a2b
Dylan Perry <dylanbperry@gmail.com>
2026-05-06 15:00:50 -0700

    Reset theme to material 3 builder base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     // Hilt
